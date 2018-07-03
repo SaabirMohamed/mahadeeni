fd70e9272152204c63d6d773d7e6c15138962fa6
saabirmohamed <saabirgmohamed@gmail.com>
2018-07-03 09:12:24 +0200

    changes navigation slightly added heading to counselling,

--- src/app/navi/navi.component.ts
+++ src/app/navi/navi.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, HostListener, ElementRef , ViewChild, ViewContainerRef} from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -11,6 +11,9 @@ import { map } from 'rxjs/operators';
 })
 export class NaviComponent {
   @Output() bgchange: EventEmitter<any> = new EventEmitter();
+  @ViewChild('navsection', {read: ViewContainerRef}) navsection: ViewContainerRef;
+  isScrolling: boolean;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
@@ -21,5 +24,27 @@ export class NaviComponent {
       this.bgchange.emit(bgname);
   }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private elem: ElementRef, private breakpointObserver: BreakpointObserver) {}
+  @HostListener('window:scroll', ['$event'])
+  onScroll($event: Event): void {
+    console.log('On Scroll');
+    // Logic To Check whether we are bottom of the page
+    // if ((window.innerHeight + window.scrollY) <= document.body.offsetHeight) {
+    //   console.log('REACHED TOP' + window.scrollY);
+    //   // Write logic here for loading new content.
+    // }
+    if (window.scrollY <= 0 ) {
+      // toggle class
+      // console.log(window.scrollY);
+      this.isScrolling = false;
+      this.elem.nativeElement.setAttribute('style', ' font-size: 1rem');
+    } else {
+      this.isScrolling  = true;
+      // console.log(window.scrollY);
+      this.elem.nativeElement.setAttribute('style', 'box-shadow: 0px 10px 13px #000 !important; font-size: 1.3rem');
+    }
+  }
+
+
+
   }
