f34a99ba908bead2bd2a6d8dabb2b0ccee5570fa
Saabir <saabirgmohamed@gmail.com>
2018-07-10 10:20:33 +0200

    post crud files added

--- src/app/modules/posts/components/posts-crud/posts-crud.component.ts
+++ src/app/modules/posts/components/posts-crud/posts-crud.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreDocument ,AngularFirestoreCollection } from 'angularfire2/firestore';
+import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs';
 @Component({
   selector: 'app-posts-crud',
@@ -11,21 +12,51 @@ export class PostsCRUDComponent implements OnInit {
   constructor(private afs: AngularFirestore) { }
 
   posts: Observable<Ipost>[];
-
+  currentIDs =  [];  
   ngOnInit() {
-    this.afs.collection<Observable<Ipost>>('/posts').valueChanges()
-    .subscribe(data => {
-      console.log(data);
+  
+    
+    
+    this.afs.collection<Observable<Ipost>>('/posts').valueChanges().subscribe(data => {
       this.posts = data;
     });
+
+    const id = this.afs.collection('/posts').snapshotChanges().subscribe(snap => {
+      snap.map(a => this.currentIDs.push(a.payload.doc.id))
+    });
+    
+     
+      
+
+  
+  }
+
+  updatePost(key: any, payload: Ipost) {
+    console.log(payload);
+    this.afs.doc('posts/'+key).update(payload).then(cb => {
+      console.log(cb);
+    }
+    ).catch( error => console.log(error));
+  }
+
+  deletePost() {
+
+  }
+
+  addNewPost() {
+
   }
 
 }
 
 export interface Ipost {
+
   title: string;
-  subtile: string;
+  subtitle: string;
   content: Iparagraphs[];
+  entryDate: string;
+  author: string;
+  path: string;
 }
 
 export interface Iparagraphs {
