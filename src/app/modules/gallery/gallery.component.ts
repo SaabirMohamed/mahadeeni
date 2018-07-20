bea04b65eae29f08c4ac6c57b64835a4a062a141
saabirmohamed <saabirgmohamed@gmail.com>
2018-07-20 15:26:27 +0200

    added dynamic video section to gallery

--- src/app/modules/gallery/gallery.component.ts
+++ src/app/modules/gallery/gallery.component.ts
@@ -12,41 +12,55 @@ import { Observable } from 'rxjs';
 })
 export class GalleryComponent implements OnInit {
 
-  images : Observable<Iimage[]>;
+  images: Observable<Iimage[]>;
   imagesArray = [];
+  videoArray = [];
   currentImage = '';
+  currentVideo = '';
 
   constructor(private svc: ServiceService) { }
   showImage(id) {
     this.currentImage = id;
-    console.log(this.currentImage);
-    console.log(id);
-    
+    // console.log(this.currentImage);
+    // console.log(id);
+
   }
 
-  hidePreview(){
+  hidePreview() {
     this.currentImage = '';
+  }
+
+  hidePreviewVideo() {
+    this.currentVideo = '';
   }
   ngOnInit() {
       this.svc.getAllImages().subscribe((data: Observable<Iimage[]>) => {
       this.images = data;
       this.makeResult();
-      
-    }
+        }
     );
+  }
+
+  showVideo(id) {
+    this.currentVideo = id;
+    console.log(this.currentVideo);
   }
 
   makeResult() {
     Object.keys(this.images).map((key) => {
-    if (this.images[key].substr(this.images[key].length - 3) === 'png') {
+    if (this.images[key].substr(this.images[key].length - 3) === 'png' ||
+        this.images[key].substr(this.images[key].length - 3) === 'jpg' ||
+        this.images[key].substr(this.images[key].length - 3) === 'gif' ) {
       console.log(this.images[key].substr(this.images[key].length - 3));
       this.imagesArray.push(this.images[key]);
     }
-    
-  }
+    if (this.images[key].substr(this.images[key].length - 3) === 'mp4') {
+        this.videoArray.push(this.images[key]);
+    }
+}
  }
 }
 export interface Iimage {
-    id:string;
+    id: string;
     name: string;
 }
